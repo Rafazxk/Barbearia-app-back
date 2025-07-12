2273e2b2c42f2656aa4a526c82cea402bca9537c
Rafael <rs2789115@gmail.com>
2025-07-12 12:51:09 -0300

    rota raiz

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const agendamentoRoutes = require('./routes/agendamentoRoute');
-app.use('/api/agendamentos', agendamentoRoutes);
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-
+// ROTA PRINCIPAL (precisa vir antes do listen)
 app.get('/', (req, res) => {
   res.send('API da Barbearia está funcionando! 💈');
 });
+
+const agendamentoRoutes = require('./routes/agendamentoRoute');
+app.use('/api/agendamentos', agendamentoRoutes);
+
+// PORTA DO FLY.IO
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
